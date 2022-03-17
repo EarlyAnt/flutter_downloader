f22a15fffb8900eda548313437ee96d924279614
EarlyAnt <54763755@qq.com>
2022-03-17 09:43:08 +0800

    1.log_file_page.dart脚本，页面右上角增加复制按钮。

--- down_loader/lib/log_file_page.dart
+++ down_loader/lib/log_file_page.dart
@@ -1,30 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:vs_scrollbar/vs_scrollbar.dart';
 
-class LogFilePage extends StatefulWidget {
-  const LogFilePage({Key? key, required this.fileContent}) : super(key: key);
+class LogFilePage extends StatelessWidget {
+  LogFilePage({Key? key, required this.fileContent}) : super(key: key);
 
   final String fileContent;
-
-  @override
-  State<LogFilePage> createState() => _LogFilePageState();
-}
-
-class _LogFilePageState extends State<LogFilePage> {
   final ScrollController _scrollController = ScrollController();
+  final ValueNotifier<String> _valueNotifier = ValueNotifier<String>("");
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("显示log文件"), centerTitle: true),
+      appBar: AppBar(
+        title: const Text("显示log文件"),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.copy),
+            onPressed: () async {
+              await Clipboard.setData(ClipboardData(text: fileContent));
+              _showMessage("内容已复制");
+            },
+          )
+        ],
+      ),
       body: VsScrollbar(
         controller: _scrollController,
-        showTrackOnHover: true, // default false
-        isAlwaysShown: true, // default false
-        scrollbarFadeDuration: const Duration(
-            milliseconds: 500), // default : Duration(milliseconds: 300)
-        scrollbarTimeToFade: const Duration(
-            milliseconds: 800), // default : Duration(milliseconds: 600)
+        showTrackOnHover: true,
+        // default false
+        isAlwaysShown: true,
+        // default false
+        scrollbarFadeDuration: const Duration(milliseconds: 500),
+        // default : Duration(milliseconds: 300)
+        scrollbarTimeToFade: const Duration(milliseconds: 800),
+        // default : Duration(milliseconds: 600)
         style: VsScrollbarStyle(
             hoverThickness: 20.0, // default 12.0
             radius: const Radius.circular(16), // default Radius.circular(8.0)
@@ -36,10 +46,27 @@ class _LogFilePageState extends State<LogFilePage> {
         child: ListView(
           controller: _scrollController,
           children: [
-            Text(widget.fileContent),
+            ValueListenableBuilder<String>(
+                valueListenable: _valueNotifier,
+                builder: (context, value, widget) {
+                  return Container(
+                      color: Colors.black45,
+                      child: value.isEmpty
+                          ? null
+                          : Text(value,
+                              textAlign: TextAlign.center,
+                              style: TextStyle(color: Colors.limeAccent)));
+                }),
+            Text(fileContent),
           ],
         ),
       ),
     );
+  }
+
+  Future<void> _showMessage(String message) async {
+    _valueNotifier.value = message;
+    await Future.delayed(const Duration(milliseconds: 1500));
+    _valueNotifier.value = "";
   }
 }
